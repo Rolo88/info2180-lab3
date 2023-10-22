e62312b19862da1d7261e820b6e74bd20148bd94
Rojre Byfield <rojreb@gmail.com>
2023-10-22 15:42:56 -0500

    Boxes light up when hovered and code checks for a winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,32 +1,66 @@
 window.onload = function () {
     const board = document.getElementById('board');
+    const status = document.getElementById('status');
     const gameState = new Array(9).fill('');
     let currentPlayer = 'X';
+    let gameOver = false;
   
     // Create the 3x3 grid and add squares
     for (let i = 0; i < 9; i++) {
       const square = document.createElement('div');
       square.classList.add('square');
       board.appendChild(square);
-    }
   
-    const squares = board.querySelectorAll('.square');
-  
-    squares.forEach((square, index) => {
       square.addEventListener('click', function () {
-        if (!gameState[index] && !isGameOver()) {
+        if (!gameState[i] && !gameOver) {
           square.textContent = currentPlayer;
           square.classList.add(currentPlayer);
-          gameState[index] = currentPlayer;
+          gameState[i] = currentPlayer;
+          
+          if (checkForWin(currentPlayer)) {
+            status.textContent = `Congratulations! ${currentPlayer} is the winner!`;
+            gameOver = true;
+          } else if (gameState.every(value => value !== '')) {
+            status.textContent = 'It\'s a draw!';
+            gameOver = true;
+          }
+  
+          // Toggle the current player after the move
           currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
         }
       });
-    });
   
-    function isGameOver() {
-      // Implement your game logic to check for a win condition here
-      // Return true if the game is over, and false otherwise
+      square.addEventListener('mouseenter', function () {
+        square.classList.add('hover');
+      });
+  
+      square.addEventListener('mouseleave', function () {
+        square.classList.remove('hover');
+      });
+    }
+  
+    function checkForWin(player) {
+      // Define your win conditions here (rows, columns, diagonals)
+      const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+      ];
+  
+      for (const condition of winConditions) {
+        const [a, b, c] = condition;
+        if (gameState[a] === player && gameState[b] === player && gameState[c] === player) {
+          return true;
+        }
+      }
+      
+      return false;
     }
   };
-
+  
 
