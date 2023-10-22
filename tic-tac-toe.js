b6ca717339e6e1bb929f09276b59959c2096c449
Rojre Byfield <rojreb@gmail.com>
2023-10-22 15:22:16 -0500

    X and O gets added to boxes alternatively when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,14 +1,32 @@
-window.onload = function(){
-    
+window.onload = function () {
     const board = document.getElementById('board');
-    const divs = board.querySelectorAll('div');
-    
-    
-    divs.forEach(div =>{
-        div.classList.add('square')
+    const gameState = new Array(9).fill('');
+    let currentPlayer = 'X';
+  
+    // Create the 3x3 grid and add squares
+    for (let i = 0; i < 9; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      board.appendChild(square);
+    }
+  
+    const squares = board.querySelectorAll('.square');
+  
+    squares.forEach((square, index) => {
+      square.addEventListener('click', function () {
+        if (!gameState[index] && !isGameOver()) {
+          square.textContent = currentPlayer;
+          square.classList.add(currentPlayer);
+          gameState[index] = currentPlayer;
+          currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+      });
     });
-
-}
-
+  
+    function isGameOver() {
+      // Implement your game logic to check for a win condition here
+      // Return true if the game is over, and false otherwise
+    }
+  };
 
 
